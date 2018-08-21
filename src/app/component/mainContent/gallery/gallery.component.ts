3890d9ebf0bad794b875242135ed3fac5e2d5c65
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2018-08-21 23:30:09 +0530

    gallery component added

--- src/app/component/mainContent/gallery/gallery.component.ts
+++ src/app/component/mainContent/gallery/gallery.component.ts
@@ -5,30 +5,77 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
 
-  images: any[];
+/*   images: any[];
   imageReady = [];
 
   ngOnInit() {
     this.images = [];
-    this.images.push({ source: 'assets/img/gallery/academic/1.jpg', alt: 'Description for Image 1', title: 'Title 1' });
-    this.images.push({ source: 'assets/img/gallery/academic/2.jpg', alt: 'Description for Image 2', title: 'Title 2' });
-    this.images.push({ source: 'assets/img/gallery/academic/3.jpg', alt: 'Description for Image 3', title: 'Title 3' });
-    this.images.push({ source: 'assets/img/gallery/academic/4.jpg', alt: 'Description for Image 4', title: 'Title 4' });
-    this.images.push({ source: 'assets/img/gallery/academic/5.jpg', alt: 'Description for Image 5', title: 'Title 5' });
-    this.images.push({ source: 'assets/img/gallery/academic/6.jpg', alt: 'Description for Image 6', title: 'Title 6' });
-    this.images.push({ source: 'assets/img/gallery/academic/7.jpg', alt: 'Description for Image 7', title: 'Title 7' });
-    this.images.push({ source: 'assets/img/gallery/academic/8.jpg', alt: 'Description for Image 8', title: 'Title 8' });
-    this.images.push({ source: 'assets/img/gallery/academic/9.jpg', alt: 'Description for Image 9', title: 'Title 9' });
-    this.images.push({ source: 'assets/img/gallery/academic/10.jpg', alt: 'Description for Image 10', title: 'Title 10' });
-    this.images.push({ source: 'assets/img/gallery/academic/11.jpg', alt: 'Description for Image 11', title: 'Title 11' });
+    this.images.push({ src: 'assets/img/gallery/academic/1.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ src: 'assets/img/gallery/academic/2.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ src: 'assets/img/gallery/academic/3.jpg', alt: 'Description for Image 3', title: 'Title 3' });
+    this.images.push({ src: 'assets/img/gallery/academic/4.jpg', alt: 'Description for Image 4', title: 'Title 4' });
+    this.images.push({ src: 'assets/img/gallery/academic/5.jpg', alt: 'Description for Image 5', title: 'Title 5' });
+    this.images.push({ src: 'assets/img/gallery/academic/6.jpg', alt: 'Description for Image 6', title: 'Title 6' });
+    this.images.push({ src: 'assets/img/gallery/academic/7.jpg', alt: 'Description for Image 7', title: 'Title 7' });
+    this.images.push({ src: 'assets/img/gallery/academic/8.jpg', alt: 'Description for Image 8', title: 'Title 8' });
+    this.images.push({ src: 'assets/img/gallery/academic/9.jpg', alt: 'Description for Image 9', title: 'Title 9' });
+    this.images.push({ src: 'assets/img/gallery/academic/10.jpg', alt: 'Description for Image 10', title: 'Title 10' });
+    this.images.push({ src: 'assets/img/gallery/academic/11.jpg', alt: 'Description for Image 11', title: 'Title 11' });
 
   }
 
   setCurrentImage(selectedImage: any): void {
     this.imageReady = this.images;
-    this.imageReady.forEach(image => { image.class = image === selectedImage ? 'active' : '' });
+    this.imageReady.forEach(image => { image.class = image === selectedImage ? 'active' : ''; });
+  } */
+
+
+  images = [];
+  selectedImage: any;
+  selecteImageIndex: number;
+
+  constructor() {
+    this.images.push({ src: 'assets/img/gallery/academic/1.jpg', alt: 'Description for Image 1', title: 'Title 1' });
+    this.images.push({ src: 'assets/img/gallery/academic/2.jpg', alt: 'Description for Image 2', title: 'Title 2' });
+    this.images.push({ src: 'assets/img/gallery/academic/3.jpg', alt: 'Description for Image 3', title: 'Title 3' });
+    this.images.push({ src: 'assets/img/gallery/academic/4.jpg', alt: 'Description for Image 4', title: 'Title 4' });
+    this.images.push({ src: 'assets/img/gallery/academic/5.jpg', alt: 'Description for Image 5', title: 'Title 5' });
+    this.images.push({ src: 'assets/img/gallery/academic/6.jpg', alt: 'Description for Image 6', title: 'Title 6' });
+    this.images.push({ src: 'assets/img/gallery/academic/7.jpg', alt: 'Description for Image 7', title: 'Title 7' });
+    this.images.push({ src: 'assets/img/gallery/academic/8.jpg', alt: 'Description for Image 8', title: 'Title 8' });
+    this.images.push({ src: 'assets/img/gallery/academic/9.jpg', alt: 'Description for Image 9', title: 'Title 9' });
+    this.images.push({ src: 'assets/img/gallery/academic/10.jpg', alt: 'Description for Image 10', title: 'Title 10' });
+    this.images.push({ src: 'assets/img/gallery/academic/11.jpg', alt: 'Description for Image 11', title: 'Title 11' });
+
   }
+
+
+
+  displayImage(i): void {
+    console.log(i, this.images[i]);
+    this.selecteImageIndex = i;
+    this.selectedImage = this.images[i];
+  }
+
+  prevImage(): void {
+    this.selecteImageIndex = this.selecteImageIndex === 0 ? this.images.length - 1 : this.selecteImageIndex - 1;
+    this.selectedImage = this.images[this.selecteImageIndex];
+  }
+
+  nextImage(): void {
+    this.selecteImageIndex = this.selecteImageIndex === this.images.length - 1 ? 0 : this.selecteImageIndex + 1;
+    this.selectedImage = this.images[this.selecteImageIndex];
+  }
+
+  imageLoad(): void {
+    console.log('imageLoading');
+  }
+
+  removeSpinner(divId: string): void {
+    document.getElementById(divId).setAttribute('style', 'display:none;');
+  }
+
 
 }
