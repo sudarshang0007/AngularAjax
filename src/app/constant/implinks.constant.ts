a0fa24569c36de0d532873b5449f471ceebe0922
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2021-11-09 13:58:22 +0530

    Vidyalaksmi link added

--- src/app/constant/implinks.constant.ts
+++ src/app/constant/implinks.constant.ts
@@ -1,7 +1,8 @@
 export const importantLinks = [
     {displayText: 'Prospectus', filename: './assets/documentation/imp/Prospectus.pdf', link: ''},
     {displayText: 'Perspective Plan 2019-2024', filename: './assets/documentation/imp/PerspectivePlan2019-2024.pdf', link: ''},
-    {displayText: 'National Academy Depository', filename: '', link: 'http://www.nad.gov.in'}
+    {displayText: 'National Academy Depository', filename: '', link: 'http://www.nad.gov.in'},
+    {displayText: 'Vidya Lakshmi Educational Loan', filename: '', link: 'https://www.vidyalakshmi.co.in/'}
 ];
 
 export const academicLinks=[
